5ee00915527825a7d73dda91d49236e89934d51d
kpacha <kpacha666@gmail.com>
2016-07-31 22:53:28 +0200

    autoscroll the logs

--- src/main/java/com/github/kpacha/tankevolver/LoggerPanel.java
+++ src/main/java/com/github/kpacha/tankevolver/LoggerPanel.java
@@ -1,20 +1,31 @@
 package com.github.kpacha.tankevolver;
 
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 
 import com.github.kpacha.tankbuilder.Individual;
 
 public class LoggerPanel extends JTextPane implements EvolutionLogger, EvolutionObserver {
 
+	private JScrollPane logsScrollPane;
+
 	public LoggerPanel(Runner runner){
 		setEditable(false);
 		runner.addLogger(this);
 		runner.addObserver(this);
+		logsScrollPane = new JScrollPane(this);
+	}
+
+	public JScrollPane getScrollPane(){
+		return logsScrollPane;
 	}
 
 	@Override
 	public void log(String msg) {
 		setText(getText() + "\n" + msg);
+		JScrollBar sb = logsScrollPane.getVerticalScrollBar();
+		sb.setValue(sb.getMaximum());
 	}
 
 	@Override
